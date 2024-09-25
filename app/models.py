6160949385557c6f1347a0d805e66eae0c1bec2d
Frankline Were <franklinegift@gmail.com>
2024-09-25 11:41:14 +0300

    Aggregate and Relationship Methods

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ Base = declarative_base()
 engine = create_engine("sqlite:///concerts.db")
 Session = sessionmaker(bind=engine)
 
+
 class Band(Base):
     __tablename__ = "bands"
 
@@ -15,13 +16,38 @@ class Band(Base):
 
     concerts = relationship("Concert", backref="band")
 
+    # Object Relationship Methods
     def get_concerts(self):
         """return a collection of all the concerts that the Band has played"""
         return self.concerts
-    
+
     def venues(self, session):
         """return a collection of all the venues the band has performed at"""
-        return session.query(Venue).join(Concert).filter(Concert.band == self).distinct().all()
+        return (
+            session.query(Venue)
+            .join(Concert)
+            .filter(Concert.band == self)
+            .distinct()
+            .all()
+        )
+
+    # Aggregate and Relationship Methods
+    def play_in_venue(self, session, venue, date):
+        """takes a venue (Venue instance) and date (as a string) as arguments creates a new concert for the band in that venue on that date"""
+        new_concert = Concert(band=self, venue=venue, date=date)
+        session.add(new_concert)
+        session.commit()
+
+    def all_introductions(self):
+        """returns an array of strings representing all the introductions for this band"""
+        return [concert.introduction() for concert in self.concerts]
+
+    @classmethod
+    def most_performances(cls, session):
+        """returns the Band instance for the band that has played the most concerts"""
+        bands = session.query(cls).all()
+        return max(bands, key=lambda band: len(band.concerts))
+
 
 class Venue(Base):
     __tablename__ = "venues"
@@ -32,13 +58,30 @@ class Venue(Base):
 
     concerts = relationship("Concert", backref="venue")
 
-    def get_concerts(self): 
+    # Object Relationship Methods
+    def get_concerts(self):
         """return a collection of concerts for that venue"""
         return self.concerts
-    
+
     def bands(self, session):
         """returns a collection of all the bands who performed at the Venue"""
-        return session.query(Band).join(Concert).filter(Concert.venue == self).distinct().all()
+        return (
+            session.query(Band)
+            .join(Concert)
+            .filter(Concert.venue == self)
+            .distinct()
+            .all()
+        )
+
+    # Aggregate and Relationship Methods
+    def concert_on(self, date):
+        return next((concert for concert in self.concerts if concert.date == date), None)
+    
+    def most_frequent_band(self):
+        band_concert_count = {}
+        for concert in self.concerts:
+            band_concert_count[concert.band] = band_concert_count.get(concert.band, 0) + 1
+        return max(band_concert_count, key=band_concert_count.get)
 
 
 class Concert(Base):
@@ -53,11 +96,18 @@ class Concert(Base):
     def band(self):
         """returns Band instance for this concert"""
         return self.band
-    
+
     def venue(self):
         """returns Venue instance for this concert"""
         return self.venue
 
     # Aggregate and Relationship Methods
+    def hometown_show(self):
+        """returns true if the concert is in the band's hometown, false if it is not"""
+        return self.venue.city == self.band.hometown
+
+    def introduction(self):
+        return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
+
 
 Base.metadata.create_all(engine)
--- app/seed.py
+++ app/seed.py
@@ -7,13 +7,18 @@ session = Session()
 # example instances
 band1 = Band(name="Le Band", hometown="Busia")
 band2 = Band(name="Sauti Sol", hometown="Kakamega")
+band3 = Band(name="Elani", hometown="Nairobi")
 venue1 = Venue(title="Uhuru Park", city="Nairobi")
 venue2 = Venue(title="Uhuru Gardens", city="Mombasa")
+venue3 = Venue(title="KICC", city="Nairobi")
 concert1 = Concert(date="1/1/2024", band=band1, venue=venue1)
 concert2 = Concert(date="1/8/2024", band=band2, venue=venue2)
 concert3 = Concert(date="1/12/2024", band=band1, venue=venue2)
+concert4 = Concert(date="1/15/2024", band=band3, venue=venue3)
+concert5 = Concert(date="1/20/2024", band=band2, venue=venue3)
+concert6 = Concert(date="1/25/2024", band=band1, venue=venue3)
 
-session.add_all([band1, band2, venue1, venue2, concert1, concert2, concert3])
+session.add_all([band1, band2, venue1, venue2, venue3, concert1, concert2, concert3, concert4, concert5, concert6])
 session.commit()
 
 # bands info
@@ -53,6 +58,41 @@ for venue in all_venues:
         print(f"  - {band.name}")
     
     print()
+
+print("Concert Methods:")
+print(f"Is concert1 a hometown show? {concert1.hometown_show()}")
+print(f"Concert1 introduction: {concert1.introduction()}")
+print()
+
+
+# Band methods
+print("Band Methods:")
+# play_in_venue
+band1.play_in_venue(session, venue3, "2/1/2024")
+print(f"Added new concert for {band1.name} at {venue3.title}")
+
+# all_introductions
+print(f"{band1.name} introductions:")
+for intro in band1.all_introductions():
+    print(intro)
+
+# most_performances
+most_performing_band = Band.most_performances(session)
+print(f"Band with most performances: {most_performing_band.name}")
+print()
+
+print("Venue Methods:")
+# concert_on
+date_to_check = "1/15/2024"
+concert_on_date = venue3.concert_on(date_to_check)
+if concert_on_date:
+    print(f"Concert at {venue3.title} on {date_to_check}: {concert_on_date.band.name}")
+else:
+    print(f"No concert at {venue3.title} on {date_to_check}")
+
+# most_frequent_band
+most_frequent_band = venue3.most_frequent_band()
+print(f"Most frequent band at {venue3.title}: {most_frequent_band.name}")
 
 # close session
 session.close()
